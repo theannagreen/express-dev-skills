e5f058c6b77f779b524f7ee04b199892db911cde
theannagreen <theannalgreen@gmail.com>
2024-03-27 18:00:29 -0400

    updated server.js added notes

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// the first arg is the "starts with" path, the paths within the route modules are combined to the "starts with" paths
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
